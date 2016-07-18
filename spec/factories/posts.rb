68288a36ffb6ac927f5597236953494c3de129c6
Junyuan Xue <junyuanxue.is@gmail.com>
2016-07-18 22:34:11 +0100

    Created view for index. Tests green! :D

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -2,6 +2,5 @@ FactoryGirl.define do
   factory :post do
     title { Faker::Book.title }
     body { Faker::Hipster.paragraph }
-    author
   end
 end
--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -13,4 +13,17 @@ describe "Posts" do
       expect(Post.count).to eq 1
     end
   end
+
+  describe "Displaying all posts" do
+    let!(:post_1) { FactoryGirl.create(:post) }
+    let!(:post_2) { FactoryGirl.create(:post) }
+
+    scenario "displays all posts" do
+      visit "/"
+      expect(page).to have_content post_1.title
+      expect(page).to have_content post_1.body
+      expect(page).to have_content post_2.title
+      expect(page).to have_content post_2.body
+    end
+  end
 end
