efb87a1bd2a1f08f5553abd261c3a6e144114864
Junyuan Xue <junyuanxue.is@gmail.com>
2016-07-18 21:15:46 +0100

    Added first feature test for creating a post

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -1,1 +1,16 @@
-require 'rails_helper'
+require "rails_helper"
+
+describe "Posts" do
+  describe "Creating a post" do
+    scenario "can create a post" do
+      visit "/"
+      click_link "New post"
+      fill_in :title, with: "Some gibberish"
+      fill_in :body, with: "Some more gibberish"
+      click_button "Submit"
+      expect(page).to have_content "Some gibberish"
+      expect(page).to have_content "Some more gibberish"
+      expect(Post.count).to eq 1
+    end
+  end
+end
