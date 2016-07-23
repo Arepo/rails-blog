50fbc4757f33b102f7e9138d12adf70a6434f0d9
Junyuan Xue <junyuanxue.is@gmail.com>
2016-07-23 12:18:03 +0100

    Added validation for title and body

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   has_many :authors, through: :contributions
   has_many :posts_tags
   has_many :tags, through: :posts_tags
+  validates :title, presence: true
+  validates :body, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -6,4 +6,7 @@ describe Post do
   it { should have_many :contributions }
   it { should have_many :authors }
   it { should have_many :posts_tags }
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+
 end
