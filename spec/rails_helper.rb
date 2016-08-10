4a0864edc589377f47df70d646bf6a59eb323d3d
Sasha Cooper <jinksy@gmail.com>
2016-08-10 17:37:46 +0100

    Move feature methods into helpers module

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 require 'capybara/rails'
+require 'helpers/feature_helpers'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -29,6 +30,7 @@ require 'capybara/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FeatureHelpers
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
