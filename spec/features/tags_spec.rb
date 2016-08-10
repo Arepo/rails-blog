eb09e33b531b2c1ca9702e111e5cbf27eb612b54
Sasha Cooper <jinksy@gmail.com>
2016-08-10 18:30:16 +0100

    Add tag creation feature spec

--- spec/features/tags_spec.rb
+++ spec/features/tags_spec.rb
@@ -12,11 +12,27 @@ describe "Tags" do
 
   scenario "Creating a tag when creating a post" do
     when_i_create_a_post
+    and_i_fill_in_all_the_fields
+    and_i_submit_new_tags
+    then_those_tags_should_be_associated_with_the_post
   end
 
   def then_i_should_see_all_their_tags
     tags = Tag.names
 
     expect(page.text).to include *tags
+  end
+
+####
+
+  def and_i_submit_new_tags
+    fill_in 'New tags', with: "Tag! You're it!"
+    and_submit_the_post
+  end
+
+  def then_those_tags_should_be_associated_with_the_post
+    expect(Post.last.tags.pluck(:name)).to include "Tag!",
+                                                   "You're",
+                                                   "it!"
   end
 end
