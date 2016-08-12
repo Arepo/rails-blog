36bf402f0ae7a0955963aec7ac76ff6dbacf7945
Sasha Cooper <jinksy@gmail.com>
2016-08-12 12:04:23 +0100

    Add name and email specs to Author

--- app/models/author.rb
+++ app/models/author.rb
@@ -2,4 +2,6 @@ class Author < ApplicationRecord
 
   has_many :contributions, dependent: :destroy
   has_many :posts, through: :contributions
+
+  validates :email, :name, :password, presence: true
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,7 +4,14 @@ describe Author do
 
   it { should have_many(:contributions).dependent(:destroy) }
   it { should have_many :posts }
-  it { should validate_presence_of :password }
   it { should validate_presence_of :email }
+  it { should validate_uniqueness_of :email }
+  it { should have_db_index :email }
   it { should validate_presence_of :name }
+
+  it "has some kind of email validation" do
+    author = Author.new(email: "unconvincingemail")
+    expect(author).not_to be_valid
+    expect(author.errors[:email]).to include('Your email is bad and you should feel bad')
+  end
 end
