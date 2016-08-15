c8cc7c74545475d3f02b4d7bec9b2c5cf98b406d
Sasha Cooper <jinksy@gmail.com>
2016-08-15 13:37:14 +0100

    Add user methods to appcontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_author
+    @current_author ||= Author.find_by(id: session[:author_id])
+  end
+
+  def logged_in?
+    !!current_author
+  end
 end
