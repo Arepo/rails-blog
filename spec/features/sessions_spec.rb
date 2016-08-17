635949279d3db2ce44a5e879b4ffaa3e4061e820
Sasha Cooper <jinksy@gmail.com>
2016-08-17 15:29:29 +0100

    Session specs green, one edge case to come

--- spec/features/sessions_spec.rb
+++ spec/features/sessions_spec.rb
@@ -2,47 +2,45 @@ require 'rails_helper'
 
 describe "Sessions", type: :feature do
   context "Logging in" do
-    scenario "Successfully" do
-      given_an_author_exists
-      when_i_visit_the_author_login_page
-      and_i_log_in_successfully
-      then_i_should_see_a_message_confirming_login
-    end
+    before { given_an_author_exists }
 
     scenario "Unsuccessfuly" do
       when_i_visit_the_author_login_page
       and_i_log_in_unsuccessfully
       then_i_should_see_a_message_denying_login
     end
+
+    scenario "Temporarily" do
+      when_i_visit_the_author_login_page
+      and_i_log_in_temporarily
+      then_i_should_see_a_message_confirming_login
+      and_i_should_not_have_my_session_stored_in_a_cookie
+    end
+
+    scenario "Permanently" do
+      when_i_visit_the_author_login_page
+      and_i_log_in_permanently
+      then_i_should_have_my_session_stored_in_a_cookie
+    end
   end
 
-  scenario "Logging out" do
-    given_i_am_logged_in
-    when_i_log_out
-    then_i_should_see_confirmation_of_logging_out
-  end
+  context "Logging out" do
+    scenario "Logging out" do
+      given_i_am_logged_in
+      when_i_log_out
+      then_i_should_see_confirmation_of_logging_out
+    end
 
-  def given_an_author_exists
-    author
+    scenario "Trying to log out twice" do
+      given_i_am_logged_in
+      when_i_log_out_twice
+      then_nothing_should_go_wrong
+    end
   end
 
   def when_i_visit_the_author_login_page
     visit login_path
   end
-
-  def and_i_log_in_successfully
-    fill_in 'Email', with: author.email
-    fill_in 'Password', with: 'unbreakable!'
-    click_button 'Log in'
-  end
-
-  def then_i_should_see_a_message_confirming_login
-    expect(page).to have_content "Hello #{author.name}, you sexy pinnacle of evolution"
-  end
-
-  let(:author) { FactoryGirl.create(:author, password: 'unbreakable!') }
-
-####
 
   def and_i_log_in_unsuccessfully
     fill_in 'Email', with: Faker::Internet.email
@@ -52,6 +50,42 @@ describe "Sessions", type: :feature do
 
   def then_i_should_see_a_message_denying_login
     expect(page).to have_content "No author found with that username/password combination"
+  end
+
+####
+
+  def given_an_author_exists
+    author
+  end
+
+  def and_i_log_in_temporarily
+    fill_in 'Email', with: author.email
+    fill_in 'Password', with: 'unbreakable!'
+    click_button 'Log in'
+  end
+
+  def then_i_should_see_a_message_confirming_login
+    expect(page).to have_content "Hello #{author.name}, you sexy pinnacle of evolution"
+  end
+
+  def and_i_should_not_have_my_session_stored_in_a_cookie
+    expect(remember_token).to be nil
+  end
+
+  let(:remember_token) { page.driver.request.cookies['remember_token'] }
+  let(:author) { FactoryGirl.create(:author, password: 'unbreakable!') }
+
+####
+
+  def and_i_log_in_permanently
+    fill_in 'Email', with: author.email
+    fill_in 'Password', with: 'unbreakable!'
+    check 'Remember me'
+    click_button 'Log in'
+  end
+
+  def then_i_should_have_my_session_stored_in_a_cookie
+    expect(remember_token).to be_present
   end
 
 ####
@@ -67,5 +101,16 @@ describe "Sessions", type: :feature do
 
   def then_i_should_see_confirmation_of_logging_out
     expect(page).to have_content "I name thee BETRAYER!"
+  end
+
+####
+
+  def when_i_log_out_twice
+    click_link 'Log out'
+    page.driver.delete logout_path
+  end
+
+  def then_nothing_should_go_wrong
+    # No-op
   end
 end
