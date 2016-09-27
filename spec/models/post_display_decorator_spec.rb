7e1b0370ef57b0065713acc23fb2bbe57bc19a76
Sasha Cooper <jinksy@gmail.com>
2016-09-27 13:48:52 +0100

    Add spec for calling original post methods from decorator

--- spec/models/post_display_decorator_spec.rb
+++ spec/models/post_display_decorator_spec.rb
@@ -1,18 +1,25 @@
 require 'rails_helper'
 
 describe PostDisplayDecorator do
-  it 'Converts title, body and topic to markdown' do
-    post = Post.new(
+  let(:post) do
+    Post.new(
       title: "Reasons why I'm **better** than Neil",
       body: "* I make seven layer bean dip of the gods" << "\n* I’m also in Halo 3 What are the odds?",
       topic: 'Important _Sience_!'
     )
-    decorator = PostDisplayDecorator.new(post)
+  end
 
+  let(:decorator) { PostDisplayDecorator.new(post) }
+
+  it 'Converts title, body and topic to markdown' do
     expect(decorator.title).to include "<strong>better</strong>"
     expect(decorator.body).to include  "<ul>",
                                   "<li>I make seven layer bean dip of the gods</li>",
                                   "<li>I’m also in Halo 3 What are the odds?</li>"
     expect(decorator.topic).to include "<em>Sience</em>"
+  end
+
+  it 'Can pass methods directly to post' do
+    expect(decorator.call_original(:title)).to eq post.title
   end
 end
