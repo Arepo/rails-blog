df24094f9aef4204740eff8d69c90e9cfb923780
Sasha Cooper <jinksy@gmail.com>
2016-09-30 01:34:36 +0100

    Add anchor links to article headers

--- app/decorators/post_display_decorator.rb
+++ app/decorators/post_display_decorator.rb
@@ -1,4 +1,6 @@
 class PostDisplayDecorator
+  include ActionView::Helpers::SanitizeHelper
+
   def self.render_multiple(strings)
     strings.map { |string| markdown.render(string).html_safe }
   end
@@ -14,7 +16,18 @@ class PostDisplayDecorator
   end
 
   def body
-    markdown.render(post.body).html_safe
+    html = markdown.render(post.body)
+    html.gsub(/(<h3>)(.*)(<\/h3)/) do
+      # TODO - Try and get this into a single operation. Tried with the regex
+      # /(<h3>)(.*)(<\/h3)|(<h4>)(.*)(<\/h4)|(<h5>)(.*)(<\/h5)/ but it returned the match data thus:
+      # #<MatchData "<h4>Mama header</h4" 1:nil 2:nil 3:nil 4:"<h4>" 5:"Mama header" 6:"</h4" 7:nil 8:nil 9:nil>
+      # for the second match, and so on for the third
+      add_anchors($1, $2, $3)
+    end.gsub(/(<h4>)(.*)(<\/h4)/) do
+      add_anchors($1, $2, $3)
+    end.gsub(/(<h5>)(.*)(<\/h5)/) do
+      add_anchors($1, $2, $3)
+    end.html_safe
   end
 
   def call_original(method)
@@ -47,6 +60,11 @@ class PostDisplayDecorator
 
   def self.markdown
     @markdown ||= Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+  end
+
+  def add_anchors(opening_tag, contents, closing_tag)
+    uri_safe_header = strip_tags(contents).parameterize
+    opening_tag + "<a id=\"#{uri_safe_header}\">" + contents + '</a>' + closing_tag
   end
 
   def markdown
