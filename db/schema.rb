8f5d98c9e39459fccbda34d3f8f4ae2d1c5fa6ad
Sasha Cooper <jinksy@gmail.com>
2024-05-24 11:32:30 +0800

    Update to Rails 6

--- db/schema.rb
+++ db/schema.rb
@@ -2,75 +2,75 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930143655) do
+ActiveRecord::Schema.define(version: 2016_09_30_143655) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "authors", force: :cascade do |t|
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-    t.text     "name"
-    t.text     "email"
-    t.string   "password_digest"
-    t.string   "remember_digest"
-    t.index ["email"], name: "index_authors_on_email", unique: true, using: :btree
-  end
-
-  create_table "contributions", force: :cascade do |t|
-    t.integer  "post_id"
-    t.integer  "author_id"
+  create_table "authors", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id"], name: "index_contributions_on_author_id", using: :btree
-    t.index ["post_id"], name: "index_contributions_on_post_id", using: :btree
+    t.text "name"
+    t.text "email"
+    t.string "password_digest"
+    t.string "remember_digest"
+    t.index ["email"], name: "index_authors_on_email", unique: true
   end
 
-  create_table "friendly_id_slugs", force: :cascade do |t|
-    t.string   "slug",                      null: false
-    t.integer  "sluggable_id",              null: false
-    t.string   "sluggable_type", limit: 50
-    t.string   "scope"
+  create_table "contributions", id: :serial, force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_contributions_on_author_id"
+    t.index ["post_id"], name: "index_contributions_on_post_id"
+  end
+
+  create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
-    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
-    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
-    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
-    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.datetime "created_at",                   null: false
-    t.datetime "updated_at",                   null: false
-    t.text     "title"
-    t.string   "body"
-    t.text     "topic"
-    t.string   "slug"
-    t.boolean  "publish",      default: false
-    t.date     "published_on"
-    t.index ["slug"], name: "index_posts_on_slug", unique: true, using: :btree
-  end
-
-  create_table "posts_tags", force: :cascade do |t|
-    t.integer  "post_id"
-    t.integer  "tag_id"
+  create_table "posts", id: :serial, force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["post_id"], name: "index_posts_tags_on_post_id", using: :btree
-    t.index ["tag_id"], name: "index_posts_tags_on_tag_id", using: :btree
+    t.text "title"
+    t.string "body"
+    t.text "topic"
+    t.string "slug"
+    t.boolean "publish", default: false
+    t.date "published_on"
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
-  create_table "tags", force: :cascade do |t|
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
-    t.text     "name",       default: ""
+  create_table "posts_tags", id: :serial, force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_posts_tags_on_post_id"
+    t.index ["tag_id"], name: "index_posts_tags_on_tag_id"
+  end
+
+  create_table "tags", id: :serial, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "name", default: ""
   end
 
 end
